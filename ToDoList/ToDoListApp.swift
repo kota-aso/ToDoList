7719c3bd69fbc7d73e2551c85443d2fbb4e6a645
kota-aso <kotaaso1212@gmail.com>
2024-03-20 21:29:36 +0900

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Kota Aso on 2024/03/20.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
